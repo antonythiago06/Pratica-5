e52a7c57b8497813d58d94a8bb70228949f12e32
antonythiago06 <naloucura545@gmail.com>
2026-06-24 19:13:34 -0300

    chore(api): Adicionar o Controller no inscricoes.module.ts

---
+++ api-inscricoes/src/inscricoes/inscricoes.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { InscricoesController } from './inscricoes.controller';
+import { InscricoesService } from './inscricoes.service';
+
+@Module({
+  controllers: [InscricoesController], 
+  providers: [InscricoesService],
+})
+export class InscricoesModule {}
